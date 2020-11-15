85d5374f3640587fb8472757759a37eef1b786cb
irshadali97 <irshadali.cv97@gmail.com>
2020-11-15 23:32:57 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+//1. Assignment1
+function result(name, percentage) {
+    console.log("My name is " + name +  " and I got " + percentage + "%");
+}
+result("Irshad", 60) 
+
+//Assignment 2 
+
+function cToF(celsius) 
+{
+  var cTemp = celsius;
+  var cToFahr = cTemp * 9 / 5 + 32;
+  var formula = cTemp+'\xB0C is ' + cToFahr + ' \xB0F.';
+    console.log(formula);
+}
+
+cToF(40);
+
+
